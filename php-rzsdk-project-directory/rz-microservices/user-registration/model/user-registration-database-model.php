7562e2ad17baf4cf6fd992ccf67154a2c587d547
Rz Rasel <emailrzrasel@gmail.com>
2024-06-28 16:42:18 +0600

    Mapping requested data and database column data

--- php-rzsdk-project-directory/rz-microservices/user-registration/model/user-registration-database-model.php
+++ php-rzsdk-project-directory/rz-microservices/user-registration/model/user-registration-database-model.php
@@ -3,6 +3,7 @@ namespace RzSDK\Model\User\Registration;
 ?>
 <?php
 use RzSDK\DatabaseSpace\UserRegistrationTable;
+use RzSDK\HTTPRequest\UserRegistrationRequest;
 use RzSDK\Model\User\Registration\UserRegistrationRequestModel;
 use RzSDK\Log\DebugLog;
 ?>
@@ -29,7 +30,44 @@ class UserRegistrationDatabaseModel {
         if(empty($keyMapping)) {
             $keyMapping = $userRegiRequestModel->arrayKeyMap();
         }
-        DebugLog::log($keyMapping);
+        //DebugLog::log($keyMapping);
+        //
+        //
+        $requestedDataSet = [];
+        foreach($keyMapping as $oldKey => $newKey) {
+            if (property_exists($userRegiRequestModel, $oldKey)) {
+                $requestedDataSet[$newKey] = $userRegiRequestModel->$oldKey;
+            }
+        }
+        //DebugLog::log($requestedDataSet);
+        //
+        //
+        $userRegiRequest = new UserRegistrationRequest();
+        $keyMapping = $userRegiRequest->keyMapping();
+        //DebugLog::log($keyMapping);
+        $databaseDataSet = [];
+        foreach($keyMapping as $oldKey => $newKey) {
+            if(array_key_exists($oldKey, $requestedDataSet)) {
+                $databaseDataSet[$newKey] = $requestedDataSet[$oldKey];
+            }
+        }
+        //DebugLog::log($databaseDataSet);
+        //
+        //
+        $userRegiTable = new UserRegistrationTable();
+        $databaseColumn = $userRegiTable->getColumn();
+        $databaseColumn = $userRegiTable->getColumnWithKey();
+        //DebugLog::log($databaseColumn);
+        //
+        /*$keys = array_keys($array);
+        $values = array_fill(0, count($keys), null);
+        $new_array = array_combine($keys, $values);*/
+        $databaseColumn = array_fill_keys(array_keys($databaseColumn), null);
+        //DebugLog::log($databaseColumn);
+        //
+        //
+        $testData = array_merge($databaseColumn, $databaseDataSet);
+        DebugLog::log($testData);
     }
 }
 ?>
