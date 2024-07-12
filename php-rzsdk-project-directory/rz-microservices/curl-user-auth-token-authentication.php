818dd62f10dba624c2594d13735957cd7debb9a7
Rz Rasel <emailrzrasel@gmail.com>
2024-07-12 13:18:14 +0600

    User Registration Request Validation Service

--- php-rzsdk-project-directory/rz-microservices/curl-user-auth-token-authentication.php
+++ php-rzsdk-project-directory/rz-microservices/curl-user-auth-token-authentication.php
@@ -58,7 +58,7 @@ class CurlUserAuthTokenAuthentication {
             "agent_type"        => "android_app",
             "user_email"        => "email@gmail.com",
             "password"          => "123456aB#",
-            "user_auth_token"   => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX3Rva2VuX2lkIjoiMTcyMDcyNzkzNzYxOTU3NjYxbDE3MjEzMzI3MzdsMTcyMDcyNzkzNzYxOTYzNTY4bDE3MjA3Mjc5MzdsMTcyMDcyNzkzNzUzNzMzMTEzbDYzMDAiLCJ1c2VyX2lwIjoiMTI3LjAuMC4xIiwidG9rZW5fc3RhcnRfZGF0ZSI6IjIwMjQtMDctMTEgMjE6NTg6NTciLCJ0b2tlbl9zdGFydF90aW1lIjoxNzIwNzI3OTM3LCJ0b2tlbl9leHBpcnlfZGF0ZSI6IjIwMjQtMDctMTggMjE6NTg6NTciLCJ0b2tlbl9leHBpcnlfdGltZSI6MTcyMTMzMjczNywidG9rZW5fcmVmcmVzaF9kYXRlIjoiMjAyNC0wNy0xMSAyMTo1ODo1NyIsInRva2VuX3JlZnJlc2hfdGltZSI6MTcyMDcyNzkzNywidXNlcl90b2tlbl9rZXkiOiJ1ZWFzaSJ9.YmL5UasV2ORDYy2Hwt5aoKp833mk-UtrXeylUPYDlkY"
+            "user_auth_token"   => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl9zdGFydF90aW1lIjoxNzIwNzY4MTk3LCJkZXZpY2VfdHlwZSI6ImFuZHJvaWQiLCJ0b2tlbl9zdGFydF9kYXRlIjoiMjAyNC0wNy0xMiAwOTowOTo1NyIsInVzZXJfdG9rZW5fa2V5IjoidWllYXMiLCJ1c2VyX3Rva2VuX2lkIjoiMTcyMDc2ODE5NzQzNTYzMjMxbDE3MjA3NjgxOTczNTIxMjc5MGwxNzIxMzcyOTk3bDE3MjA3NjgxOTc0MzQ5ODAxMmwxNzIwNzY4MTk3bDUyMzMiLCJhZ2VudF90eXBlIjoiYW5kcm9pZF9hcHAiLCJ0b2tlbl9yZWZyZXNoX2RhdGUiOiIyMDI0LTA3LTEyIDA5OjA5OjU3IiwidG9rZW5fZXhwaXJ5X2RhdGUiOiIyMDI0LTA3LTE5IDA5OjA5OjU3IiwidG9rZW5fZXhwaXJ5X3RpbWUiOjE3MjEzNzI5OTcsInVzZXJfaXAiOiIxMjcuMC4wLjEiLCJ0b2tlbl9yZWZyZXNoX3RpbWUiOjE3MjA3NjgxOTd9.8dC80IDySF8lrG08bAZWdvPeoAQ4r70kzyURaXYqvdw"
         );
     }
 
